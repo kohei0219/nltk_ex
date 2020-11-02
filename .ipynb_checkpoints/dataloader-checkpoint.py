fb93bcbbf73011053b064e36e6908225c5aef296
unknown <02tennis19rf@gmail.com>
2020-11-02 16:43:51 +0900

    initial commit

---
+++ .ipynb_checkpoints/dataloader-checkpoint.py
@@ -0,0 +1,10 @@
+import nltk
+nltk.download('twitter_samples')
+# import word tokenization
+nltk.download('punkt')
+# import pos tagger
+nltk.download('averaged_perceptron_tagger')
+# import Lemmatisation
+nltk.download('wordnet')
+# import stop words
+nltk.download('stopwords')
